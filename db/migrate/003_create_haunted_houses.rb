2549d455a145a6bd00e7f3a1d6de061909da0fb7
Brittany Javalera <brittjavalera@gmail.com>
2019-08-26 21:46:57 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
-# Create your haunted_houses migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.decimal :price
+      t.
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :
+    end
+end
