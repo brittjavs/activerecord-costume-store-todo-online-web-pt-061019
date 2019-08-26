e87c1f5076a8ae738377d01af34367635c9e78f3
Brittany Javalera <brittjavalera@gmail.com>
2019-08-26 21:38:47 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,8 @@ class CostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.timestamps
+      t.boolean :still_in_business
+      
     end
   end
 end
