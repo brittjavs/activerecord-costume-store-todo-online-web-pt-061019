3a33c2b0a75af0c1b373f32a4dad971d2dbe45d5
Brittany Javalera <brittjavalera@gmail.com>
2019-08-26 21:33:29 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,8 +9,9 @@ class CreateCostumes < ActiveRecord::Migration
     create_table :costumes do |t|
       t.string :name
       t.decimal :price
-      t.text :size
-      t.text :image_url
+      t.string :size
+      t.string :image_url
+      t.timestamps
     end
   end
 end
